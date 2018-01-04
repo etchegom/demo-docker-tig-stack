de4ffa6bc32c8318233336c7b807b1402b10f804
Matthieu Etchegoyen <etchegom@gmail.com>
2018-01-04 21:31:18 +0100

    fix environment issue

--- grafana/entrypoint.sh
+++ grafana/entrypoint.sh
@@ -5,21 +5,25 @@ GRAFANA_URL="http://$GF_SECURITY_ADMIN_USER:$GF_SECURITY_ADMIN_PASSWORD@localhos
 post() {
     curl -s -X POST -d "$1" \
         -H 'Content-Type: application/json;charset=UTF-8' \
-        "$GRAFANA_URL$2" 2> /dev/null
+        "${GRAFANA_URL}$2" 2> /dev/null
 }
 
 if [ ! -f "/var/lib/grafana/.init" ]; then
     exec /run.sh $@ &
 
-    until curl -s "$GRAFANA_URL/api/datasources" 2> /dev/null; do
+    until curl -s "${GRAFANA_URL}/api/datasources" 2> /dev/null; do
+        echo "wait for grafana to be ready ..."
         sleep 1
     done
+    echo "grafana is ready."
 
     for DATASOURCE in /etc/grafana/datasources/*; do
+        echo "POST datasource spec ${DATASOURCE}"
         post "$(envsubst < ${DATASOURCE})" "/api/datasources"
     done
 
     for DASHBOARD in /etc/grafana/dashboards/*; do
+        echo "POST dashboard spec ${DASHBOARD}"
         post "$(cat ${DASHBOARD})" "/api/dashboards/db"
     done
 
